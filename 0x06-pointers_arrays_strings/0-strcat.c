35bd6bd6c52ce57bed87dcde6c7e5e1a62b7f5a1
Ivy-Jepkirui <penelopeivy45@gmail.com>
2023-07-28 06:03:39 -0700

    Add func

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,31 +1,30 @@
 #include "main.h"
 
 /**
- * _strncat - concatenates n bytes from a string to another
- * @dest: destination string
- * @src: source string
- * @n: number of bytes of str to concatenate
+ * _strcat - concatenates two strings
+ * @dest: string to append to
+ * @src: string to add
  *
- * Return: a pointer to the resulting string dest
+ * Return: a pointer to the resulting string
  */
-char *_strncat(char *dest, char *src, int n)
+char *_strcat(char *dest, char *src)
 {
 	int i, j;
 
-		i = 0;
-		j = 0;
+	i = 0;
+	j = 0;
 
-		while (dest[i] != '\0')
-			i++;
-		while (src[j] != '\0' && j < n)
-		{
-			dest[i] = src[j];
-			i++;
-			j++;
+	while (dest[i] != '\0')
+		i++;
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		 j++;
+		 i++;
 
-		}
+	}
 
-		dest[i] = '\0';
+	dest[i] = '\0';
 
-		return (dest);
+	return (dest);
 }
